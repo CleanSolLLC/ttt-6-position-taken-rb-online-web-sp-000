2666f90216a76376f8f65cb7aa3d12b338b30dc1
Mark May <mamark90@gmail.com>
2020-06-19 14:10:27 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,9 +2,7 @@ require 'pry'
 def position_taken?(board, index)
   pos = index - 1
 
-  if board[pos] == "" || board[pos] == " "
-    true
-  elsif board[pos] == "X" || board[pos] == "O"
+  if board[pos] == "X" || board[pos] == "O"
     true
   else
     false
